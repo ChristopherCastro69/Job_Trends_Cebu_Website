ca0bed1ecf88aafb55d87f109eec52ea44d9ab46
Topitops69 <topitops123@gmail.com>
2023-08-09 01:47:58 +0800

    Small steps lang muna

---
+++ src/Data.js
@@ -0,0 +1,36 @@
+export const UserData = [
+    {
+        id: 1,
+        year: 2016,
+        userGain : 8000,
+        userLost: 823,
+    },
+    {
+        id: 2,
+        year: 2019,
+        userGain : 4567,
+        userLost: 341,
+    },
+    {
+        id: 3,
+        year: 2020,
+        userGain : 231,
+        userLost: 8123,
+    },
+    {
+        id: 4,
+        year: 2021,
+        userGain : 8230,
+        userLost: 800,
+    },
+
+
+    {
+        id: 5,
+        year: 2023,
+        userGain : 1231,
+        userLost: 232,
+    },
+
+]
+  
